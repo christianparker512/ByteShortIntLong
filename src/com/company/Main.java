238065b023e3145ba26fa95970b192bee8a0098c
Christian Parker <christianparker512@gmail.com>
2021-09-01 10:21:09 -0500

    min and max value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,5 +13,7 @@ public class Main {
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
         System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        int myMaxIntTest = 2147483647;
     }
 }
