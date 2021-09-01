6afa9a767dcf8e199b9e4ff434abbc6f9c47ea7f
Christian Parker <christianparker512@gmail.com>
2021-09-01 10:18:12 -0500

    maxim and minimum value

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //create a variable called myValue with an int and a value of 10000;
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+//        System.out.println("Busted MIN value = " = (myMinIntValue - 1));
     }
 }
